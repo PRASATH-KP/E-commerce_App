18e1bdf6b0468ca641a7da56683586fe141b1fcb
Prasath <prasath10416@gmail.com>
2025-08-01 13:47:37 +0530

    Conflicts resolved

--- Program.cs
+++ Program.cs
@@ -3,73 +3,107 @@
 
     internal class Program
     {
-        public static List<DepositData> depositDatas = new List<DepositData>();
-        public static List<WithdrawData> withdrawDatas = new List<WithdrawData>();
-        public static List<UserDetails> UserDetails = new List<UserDetails>();
-        public static List<ProductDetails> productDetails = new List<ProductDetails>();
 
+
+        public static RegistrationDetails _currentUser;
         public static void Main(string[] args)
         {
 
-            UserDetails.Add(new UserDetails(1000, "p@gmail.com"));
-            UserDetails.Add(new UserDetails(2000, "o@gmail.com"));
-            UserDetails.Add(new UserDetails(3000, "l@gmail.com"));
-            UserDetails.Add(new UserDetails(400, "s@gmail.com"));
-            UserDetails.Add(new UserDetails(100, "a@gmail.com"));
-            UserDetails.Add(new UserDetails(900, "d@gmail.com"));
-            UserDetails.Add(new UserDetails(800, "f@gmail.com"));
-
-            productDetails.Add(new ProductDetails(10, 600));
-            productDetails.Add(new ProductDetails(20, 500));
-            productDetails.Add(new ProductDetails(90, 400));
-            productDetails.Add(new ProductDetails(50, 200));
-
-            foreach (var product in productDetails)
-            {
-                Console.Write($"Product ID : {product.ProductID} ");
-                Console.Write(" Product Count : {0} ", product.Count);
-                Console.Write(" Product Price : " + product.PriceOfProduct);
-                Console.WriteLine();
 
 
+  
+            bool loginCheck=true;
+            int option;
 
-            }
-
-            foreach (var user in UserDetails)
-            {
-                Console.Write($"User ID : {user.UserId}");
-                Console.Write(" ");
-                Console.Write($"User Balance : {user.WalletBalance}");
-                Console.Write(" ");
-                Console.Write($"User MailId : {user.MailId}");
-                Console.WriteLine();
-            }
-
-
-            foreach (var user in UserDetails)
+            Console.WriteLine("Welcome to E-Commerce App");
+            Console.WriteLine("Choose the oprtions below to proceed furhter \n 1.Login as existing User \n 2.Register as New User \n 3.Others ");
+            do 
             {
 
-                //if (user.UserId % 2 == 0)
-                //{
-                //   DepositData deposit = new DepositData(user.UserId, 100, DateTime.Now);
-                //   depositDatas.Add(deposit);
-                //   user.WalletBalance= deposit.DepositAmmount(100, DateTime.Now, user.WalletBalance);
-                //}
-                //else 
-                //{
-                //   WithdrawData withdraw = new WithdrawData(user.UserId, 20, DateTime.Now);
-                //   withdrawDatas.Add(withdraw);
-                //   user.WalletBalance= withdraw.WithdrawAmmount(20, DateTime.Now, user.WalletBalance);
-                //}
-                //Console.Write($"User ID : {user.UserId}");
-                //Console.Write(" ");
-                //Console.Write($"User Balance : {user.WalletBalance}");
-                //Console.WriteLine();
+                loginCheck = int.TryParse(Console.ReadLine(), out option);
+                if ((loginCheck && (option >= 1 && option <= 3)))
+                    break;
+                Console.WriteLine("You have given worng option");
             }
+            while (!(loginCheck &&(option>=1 && option<=3)));
+
+            switch (option)
+            {
+                case 1:
+                    _currentUser = login();
+                    MainMenu();
+                    break;
+                case 2:
+                    _currentUser = Regsiter();
+                    MainMenu();
+                    break;
+                //case 3:
+                    
+            };
+
+            
+
+            //foreach (var person in users)
+            //{
+            //    Console.Write($"Role of the Person : {person.UserRole}");
+            //    Console.Write(" ");
+            //    Console.Write($" ID : {person.Id}");
+            //    Console.Write(" ");
+            //    Console.Write($" Name : {person.Name}");
+            //    Console.Write(" ");
+            //    Console.Write($" Balance : {person.WalletBalance}");
+            //    Console.Write(" ");
+            //    Console.Write($" MailId : {person.MailId}");
+            //    Console.WriteLine();
+            //}
+
+            //foreach (var product in products)
+            //{
+            //    Console.Write($"Product ID : {product.ProductID} ");
+            //    Console.Write(" Product Count : {0} ", product.Quantity);
+            //    Console.Write(" Product Price : " + product.PriceOfProduct);
+            //    Console.WriteLine();
+            //}
 
 
-            Console.ReadLine();
+            //foreach (var user in UserDetails)
+            //{
 
+            //    //if (user.UserId % 2 == 0)
+            //    //{
+            //    //   DepositData deposit = new DepositData(user.UserId, 100, DateTime.Now);
+            //    //   depositDatas.Add(deposit);
+            //    //   user.WalletBalance= deposit.DepositAmmount(100, DateTime.Now, user.WalletBalance);
+            //    //}
+            //    //else 
+            //    //{
+            //    //   WithdrawData withdraw = new WithdrawData(user.UserId, 20, DateTime.Now);
+            //    //   withdrawDatas.Add(withdraw);
+            //    //   user.WalletBalance= withdraw.WithdrawAmmount(20, DateTime.Now, user.WalletBalance);
+            //    //}
+            //    //Console.Write($"User ID : {user.UserId}");
+            //    //Console.Write(" ");
+            //    //Console.Write($"User Balance : {user.WalletBalance}");
+            //    //Console.WriteLine();
+            //}
+
+
+            //Console.ReadLine();
+
+        }
+
+        private static void MainMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static RegistrationDetails login()
+        {
+            return null;
+        }
+        public static RegistrationDetails Regsiter()
+        {
+            return null;
         }
     }
 }
---
+++ RegistarationDetails.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApp
+{
+    public enum Role
+    {
+        Manager,
+        Admin,
+        User
+    }
+    public class RegistrationDetails
+    {
+       
+        private string _iD;
+        public string Id 
+        { 
+            get 
+            { 
+                return _iD; 
+            } 
+        }
+
+        private static int user_Id = 100;
+        private static int admin_Id = 100;
+        private static int manager_Id = 100;
+
+        public Role UserRole { get; set; }
+        public string Name { get; set; }
+
+        public string MailId { get; set; }
+
+        public decimal? WalletBalance { get; set; }
+        
+        public RegistrationDetails(Role userRole, string name, string mailID)
+        {
+            if (userRole == Role.User)
+                throw new ArgumentException("Use the user constructor for non-admin roles");
+
+            UserRole = userRole;
+            if(userRole == Role.Admin)
+            {
+                admin_Id++;
+                _iD = "AdminId" + admin_Id;
+            Name = name;
+                MailId = mailID;
+            }
+            if (userRole == Role.Manager)
+            {
+                manager_Id++;
+                _iD = "ManagerId" + manager_Id;
+                Name = name;
+                MailId = mailID;
+            }
+
+            WalletBalance = null;
+
+        }
+
+        // User Constructor
+        public RegistrationDetails(string name, string mailID, decimal walletBalance)
+        {
+            UserRole = Role.User;
+            user_Id++;
+            _iD = "UserId" + user_Id;
+            Name = name;
+            WalletBalance = walletBalance;
+            MailId = mailID;
+        }
+
+    }
+}
